18d5908c0c319159994fb70d95a3a4a72ba8f488
amitpauldev <amitpaul.per@gmail.com>
2026-09-25 19:05:13 +0600

    Fixed responsiveness of Hero Section

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,11 +4,11 @@ import banner from "../../public/assets/banner.png";
 const Hero = () => {
 	return (
 		<section className="container mx-auto py-12 px-8">
-			<div className="rounded-2xl border border-stroke-muted bg-card flex flex-col md:flex-row items-center justify-between p-10 md:p-15">
-				<div className="w-1/2">
+			<div className="rounded-2xl border border-stroke-muted bg-card flex flex-col md:flex-row items-center justify-between p-5 md:p-15">
+				<div className="w-full md:w-1/2 text-center md:text-left">
 					<span className="text-sm  text-primary">WORKOUT LIBRARY</span>
-					<h1 className="leading-none my-5">
-						TRAIN WITH INTENT. LOG <br /> EVERY SET.
+					<h1 className="leading-none my-5 text-5xl md:text-4xl lg:text-5xl xl:text-6xl">
+						TRAIN WITH INTENT. LOG EVERY SET.
 					</h1>
 					<p className="mb-11 text-muted">
 						Find your workouts, plan your day, and track your progress. Whether
@@ -20,8 +20,13 @@ const Hero = () => {
 					</button>
 				</div>
 
-				<div className="w-1/2">
-					<Image src={banner} alt="banner" width={400} className="ml-auto" />
+				<div className="w-full md:w-1/2 mt-16 md:mt-0">
+					<Image
+						src={banner}
+						alt="banner"
+						width={400}
+						className="mx-auto md:mr-0 md:ml-auto"
+					/>
 				</div>
 			</div>
 		</section>
